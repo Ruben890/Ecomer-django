be691a0bfcd1ba7579901f7cbeae37d74c682244
Darlin Ruben Nina C. <chinausto@gmail.com>
2023-12-11 16:26:03 -0400

    register

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -5,44 +5,60 @@ from django.contrib.auth.forms import AuthenticationForm
 
 class LoginForm(AuthenticationForm):
     username = forms.EmailField(
-        widget=forms.EmailInput(attrs={'class': 'border p-2 w-full', 'placeholder': 'Email'})
+        widget=forms.EmailInput(attrs={'class': 'border p-2 w-full', 'placeholder': 'Email'}),
+        label='Email'
     )
     password = forms.CharField(
-        widget=forms.PasswordInput(attrs={'class': 'border p-2 w-full', 'placeholder': 'Password'})
+        widget=forms.PasswordInput(attrs={'class': 'border p-2 w-full', 'placeholder': 'Password'}),
+        label='Password'
     )
 
 class CreateUsersForm(forms.ModelForm):
+    password_confirmation = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'border p-2 w-full', 'placeholder': 'Confirm Password'}),
+        label='Confirm Password'
+    )
+
     class Meta:
         model = Profiles
         fields = ['email', 'first_name', 'last_name', 'image', 'Tel', 'gender', 'roles', 'country', 'address']
 
     email = forms.EmailField(
-        widget=forms.EmailInput(attrs={'class': 'border p-2 w-full', 'placeholder': 'Email'})
+        widget=forms.EmailInput(attrs={'class': 'border p-2 w-full', 'placeholder': 'Email'}),
+        label='Email'
     )
     
     password = forms.CharField(
-        widget=forms.PasswordInput(attrs={'class': 'border p-2 w-full', 'placeholder': 'Password'})
+        widget=forms.PasswordInput(attrs={'class': 'border p-2 w-full', 'placeholder': 'Password'}),
+        label='Password'
     )
 
     first_name = forms.CharField(
-        max_length=100,  widget=forms.TextInput(attrs={'class': 'border p-2 w-full', 'placeholder': 'First Name'})
+        max_length=100,  widget=forms.TextInput(attrs={'class': 'border p-2 w-full', 'placeholder': 'First Name'}),
+        label='First Name'
     )
     last_name = forms.CharField(
-        max_length=100, widget=forms.TextInput(attrs={'class': 'border p-2 w-full', 'placeholder': 'Last Name'})
+        max_length=100, widget=forms.TextInput(attrs={'class': 'border p-2 w-full', 'placeholder': 'Last Name'}),
+        label='Last Name'
     )
     image = forms.ImageField(
-        required=False, widget=forms.FileInput(attrs={'class': 'border p-2 w-full mb-4'})  # Added margin-bottom
+        required=False, widget=forms.FileInput(attrs={'class': 'border p-2 w-full mb-4'}),
+        label='Profile Image'
     )
     Tel = forms.CharField(
-        max_length=10, widget=forms.TextInput(attrs={'class': 'border p-2 w-full', 'placeholder': 'Phone', 'type': 'tel'})
+        max_length=10, widget=forms.TextInput(attrs={'class': 'border p-2 w-full', 'placeholder': 'Phone', 'type': 'tel'}),
+        label='Phone'
     )
-    gender = forms.CharField(
-        widget=forms.Select(choices=GENDER_CHOICES, attrs={'class': 'border p-2 w-full'})
+    gender = forms.ChoiceField(
+        choices=GENDER_CHOICES, widget=forms.Select(attrs={'class': 'border p-2 w-full'}),
+        label='Gender'
     )
     
     country = forms.CharField(
-        max_length=150, widget=forms.TextInput(attrs={'class': 'border p-2 w-full', 'placeholder': 'Country'})
+        max_length=150, widget=forms.TextInput(attrs={'class': 'border p-2 w-full', 'placeholder': 'Country'}),
+        label='Country'
     )
     address = forms.CharField(
-        max_length=150, widget=forms.TextInput(attrs={'class': 'border p-2 w-full', 'placeholder': 'Address'})
+        max_length=150, widget=forms.TextInput(attrs={'class': 'border p-2 w-full', 'placeholder': 'Address'}),
+        label='Address'
     )
